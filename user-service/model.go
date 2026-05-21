12410ffd9146f8ea94be75adac6bacf3bf51c96d
Dhiya Ulhaq <dhiyau27@upi.edu>
2026-05-21 21:39:46 +0700

    Update model.go

--- user-service/model.go
+++ user-service/model.go
@@ -1,11 +1,11 @@
 package main
 
 type User struct {
-	UserID      int
-	Name        string
-	Email       string
-	Password    string
-	Role        string
-	Alamat      string
-	Preferensi  string
+	UserID     int    `json:"user_id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Password   string `json:"-"` // Tag "-" memberitahu Go untuk mengabaikan field ini saat konversi ke JSON
+	Alamat     string `json:"alamat"`
+	Preferensi string `json:"preferensi"`
+	Role       string `json:"role"`
 }
