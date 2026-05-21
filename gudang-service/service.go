ff57cbf1cf274286635f7b04585ae0f9f763769d
Dhiya Ulhaq <dhiyau27@upi.edu>
2026-05-22 01:02:33 +0700

    Update service.go

--- gudang-service/service.go
+++ gudang-service/service.go
@@ -5,30 +5,16 @@ import (
 	"time"
 )
 
-type SortingService struct {
-}
+type SortingService struct{}
 
 func NewSortingService() *SortingService {
 	return &SortingService{}
 }
 
-// StartSorting - Mulai proses sorting package
 func (s *SortingService) StartSorting(pkg *Package) error {
 
 	if pkg == nil {
 		return errors.New("package nil")
-	}
-
-	if pkg.Resi == "" {
-		return errors.New("resi kosong")
-	}
-
-	if pkg.WarehouseZone == "" {
-		return errors.New("warehouse kosong")
-	}
-
-	if pkg.Status != "pending" {
-		return errors.New("status tidak valid")
 	}
 
 	pkg.Status = "sorting"
@@ -36,15 +22,10 @@ func (s *SortingService) StartSorting(pkg *Package) error {
 	return nil
 }
 
-// CompleteSorting - Selesaikan proses sorting
 func (s *SortingService) CompleteSorting(pkg *Package) error {
 
 	if pkg == nil {
 		return errors.New("package nil")
-	}
-
-	if pkg.Status != "sorting" {
-		return errors.New("status tidak sorting")
 	}
 
 	now := time.Now()
@@ -55,13 +36,11 @@ func (s *SortingService) CompleteSorting(pkg *Package) error {
 	return nil
 }
 
-// GetPendingPackages - Ambil semua package dengan status pending
 func (s *SortingService) GetPendingPackages(packages []Package) []Package {
 
 	var result []Package
 
 	for _, pkg := range packages {
-
 		if pkg.Status == "pending" {
 			result = append(result, pkg)
 		}
@@ -70,27 +49,10 @@ func (s *SortingService) GetPendingPackages(packages []Package) []Package {
 	return result
 }
 
-// ValidatePackage - Validasi package data
 func (s *SortingService) ValidatePackage(pkg *Package) error {
 
 	if pkg == nil {
 		return errors.New("package nil")
-	}
-
-	if pkg.Resi == "" {
-		return errors.New("resi kosong")
-	}
-
-	if pkg.UserID <= 0 {
-		return errors.New("user id invalid")
-	}
-
-	if pkg.Berat <= 0 {
-		return errors.New("weight invalid")
-	}
-
-	if pkg.WarehouseZone == "" {
-		return errors.New("warehouse kosong")
 	}
 
 	return nil
