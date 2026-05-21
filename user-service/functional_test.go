9ff0d43866b688e74f60f02a47e614d94caf1afd
Dhiya Ulhaq <dhiyau27@upi.edu>
2026-05-21 19:31:56 +0700

    Update database connection details in tests

--- user-service/functional_test.go
+++ user-service/functional_test.go
@@ -20,7 +20,7 @@ import (
 const (
 	dbUser = "root"
 	dbPass = "root"
-	dbHost = "mysql"
+	dbHost = "localhost"
 	dbPort = "3306"
 	dbName = "tubesdb"
 )
@@ -75,7 +75,7 @@ func TestUserFlow_Functional(t *testing.T) {
 	)
 
 	respReg, err := http.Post(
-		"http://user-service:8081/register",
+		"http://localhost:8081/register",
 		"application/json",
 		bytes.NewBuffer([]byte(fmt.Sprintf(`{
 			"Name":"Functional",
@@ -105,7 +105,7 @@ func TestUserFlow_Functional(t *testing.T) {
 	// 5. LOGIN
 	// ==================================
 	respLogin, err := http.Post(
-		"http://user-service:8081/login",
+		"http://localhost:8081/login",
 		"application/json",
 		bytes.NewBuffer([]byte(fmt.Sprintf(`{
 			"Email":"%s",
@@ -135,7 +135,7 @@ func TestUserFlow_Functional(t *testing.T) {
 	reqProfile, _ := http.NewRequest(
 		"GET",
 		fmt.Sprintf(
-			"http://user-service:8081/profile?id=%d",
+			"http://localhost:8081/profile?id=%d",
 			userID,
 		),
 		nil,
