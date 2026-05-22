3037e193f7b6c105a5afdf14888fae5c5a7e5aee
Xeramu <umarexandromeda@upi.edu>
2026-05-22 08:54:13 +0700

    Update functional_test.go

--- order-service/functional_test.go
+++ order-service/functional_test.go
@@ -1,152 +1,3 @@
-// // !!!!!!!!!!!WARNING!!!!!!!!!!!!!
-// // !!!!!!!!!!!WARNING!!!!!!!!!!!!!
-// // !!!!!!!!!!!WARNING!!!!!!!!!!!!!
-
-// // Kalau mau tes lokal pake localhost,
-// // ganti semua user-service/ order-service jadi localhost
-// // portnya msh sama. 8081 atau 8083
-
-// // Kalau mau tes lokal pake docker,
-// // ganti semua localhost jadi user-service/ order-service
-// // portnya msh sama. 8081 atau 8083
-
-// //go:build functional
-
-// package main
-
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"net/http"
-// 	"testing"
-// 	"time"
-// )
-
-// func TestCreateOrder_Functional(t *testing.T) {
-
-// 	go func() {
-// 		http.HandleFunc("/order", createOrderHandler)
-// 		http.ListenAndServe(":8083", nil)
-
-// 	}()
-
-// 	time.Sleep(1 * time.Second)
-
-// 	// email unik biar tidak bentrok
-// 	email := fmt.Sprintf("func%d@mail.com", time.Now().UnixNano())
-
-// 	// ====================
-// 	// REGISTER
-// 	// ====================
-// 	respReg, err := http.Post(
-// 		"http://user-service:8081/register",
-// 		"application/json",
-// 		bytes.NewBuffer([]byte(fmt.Sprintf(`{
-// 			"Name":"Func",
-// 			"Email":"%s",
-// 			"Password":"123",
-// 			"Role":"customer"
-// 		}`, email))),
-// 	)
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	var reg map[string]interface{}
-// 	json.NewDecoder(respReg.Body).Decode(&reg)
-
-// 	userID := int(reg["user_id"].(float64))
-
-// 	// ====================
-// 	// LOGIN
-// 	// ====================
-// 	respLogin, err := http.Post(
-// 		"http://user-service:8081/login",
-// 		"application/json",
-// 		bytes.NewBuffer([]byte(fmt.Sprintf(`{
-// 			"Email":"%s",
-// 			"Password":"123"
-// 		}`, email))),
-// 	)
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	var login map[string]string
-// 	json.NewDecoder(respLogin.Body).Decode(&login)
-
-// 	token := login["token"]
-
-// 	// ====================
-// 	// TEST PROFILE DIRECT
-// 	// ====================
-// 	t.Log("Testing profile directly...")
-
-// 	reqCheck, _ := http.NewRequest(
-// 		"GET",
-// 		fmt.Sprintf("http://user-service:8081/profile?id=%d", userID),
-// 		nil,
-// 	)
-
-// 	reqCheck.Header.Set("Authorization", "Bearer "+token)
-
-// 	respCheck, err := http.DefaultClient.Do(reqCheck)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	t.Log("PROFILE STATUS DIRECT:", respCheck.StatusCode)
-
-// 	// ====================
-// 	// CREATE ORDER
-// 	// ====================
-// 	body := []byte(fmt.Sprintf(`{
-// 		"user_id":%d,
-// 		"nama_barang":"Laptop",
-// 		"berat":2,
-// 		"dimensi":"10x10",
-// 		"jenis":"Elektronik",
-// 		"alamat_pengirim":"Bandung",
-// 		"alamat_penerima":"Jakarta"
-// 	}`, userID))
-
-// 	req, _ := http.NewRequest(
-// 		"POST",
-// 		"http://order-service:8083/order",
-// 		bytes.NewBuffer(body),
-// 	)
-
-// 	req.Header.Set("Authorization", "Bearer "+token)
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	var result map[string]interface{}
-// 	json.NewDecoder(resp.Body).Decode(&result)
-
-// 	t.Log("REGISTER:", reg)
-// 	t.Log("USER ID:", userID)
-// 	t.Log("TOKEN:", token)
-// 	t.Log("ORDER RESPONSE:", result)
-// 	t.Log("STATUS:", resp.StatusCode)
-
-// 	if resp.StatusCode != 200 {
-// 		t.Fatalf("failed: %+v", result)
-// 	}
-
-// 	if result["status"] != "created" {
-// 		t.Fatalf("unexpected response: %+v", result)
-// 	}
-
-// }
-
 package main
 
 import (
@@ -161,16 +12,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// ===============================
-// CONFIG
-// ===============================
-// Kalau docker compose:
-// host = mysql
-//
-// Kalau lokal:
-// host = localhost
-//
-// Sesuaikan kalau perlu.
+// config
 const (
 	dbUser = "root"
 	dbPass = "root"
@@ -181,9 +23,7 @@ const (
 
 func TestCreateOrder_Functional(t *testing.T) {
 
-	// ==================================
-	// 1. CEK DATABASE BISA DIAKSES
-	// ==================================
+	// cek database bisa akses
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		dbUser, dbPass, dbHost, dbPort, dbName)
 
@@ -200,9 +40,7 @@ func TestCreateOrder_Functional(t *testing.T) {
 
 	t.Log("DATABASE CONNECTED")
 
-	// ==================================
-	// REGISTER USER
-	// ==================================
+	// register user
 	email := fmt.Sprintf("func%d@mail.com", time.Now().UnixNano())
 
 	respReg, err := http.Post(
@@ -225,9 +63,7 @@ func TestCreateOrder_Functional(t *testing.T) {
 
 	userID := int(reg["user_id"].(float64))
 
-	// ==================================
-	// 4. LOGIN
-	// ==================================
+	// login
 	respLogin, err := http.Post(
 		"http://host.docker.internal:8081/login",
 		"application/json",
@@ -246,9 +82,7 @@ func TestCreateOrder_Functional(t *testing.T) {
 
 	token := login["token"]
 
-	// ==================================
-	// 5. CREATE ORDER
-	// ==================================
+	// create order
 	body := []byte(fmt.Sprintf(`{
 		"user_id":%d,
 		"nama_barang":"Laptop",
@@ -281,9 +115,7 @@ func TestCreateOrder_Functional(t *testing.T) {
 		t.Fatalf("failed: %+v", result)
 	}
 
-	// ==================================
-	// 6. CEK DATA MASUK KE DATABASE
-	// ==================================
+	// cek data masuk database apa engga
 	var count int
 
 	err = db.QueryRow(
